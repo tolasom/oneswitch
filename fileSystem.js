6fc552d7cf4aea24a1d1ae7f6c17875e7f53eafe
tolasom <somtola16@kit.edu.kh>
2018-06-15 09:06:17 +0700

    file system

---
+++ fileSystem.js
@@ -0,0 +1,85 @@
+const SftpUpload = require('sftp-upload');
+const Client = require('ssh2-sftp-client');
+const sftp = new Client();
+const fs = require('fs');
+const REMOTE_URL = '/SecureBackup/Test/';
+const FILE_PATH ='/home/tolasom/Desktop/oneswitch/localfilepath';
+
+
+/**
+ * @param config        {String} is used ssh2-sftp-client lib for read and get files
+ * @param sftp_config   {String} is used to upload files to sftp server
+ */
+
+
+const config = {
+    host : '10.1.1.2',
+    port : '22',
+    username : 'ftp01',
+    password : 'AI789&*()'
+};
+const sftp_config = {
+    host : '10.1.1.2',
+    port : '22',
+    username : 'ftp01',
+    password : 'AI789&*()',
+    path: '/home/tolasom/Desktop/oneswitch/localfilepath',
+    remoteDir: '/SecureBackup/Test'
+
+};
+
+const UploadSftp = new SftpUpload(sftp_config);
+
+
+
+module.exports.uploadFile = () =>{
+    return new Promise((resolve,reject)=>{
+        fs.readdir(FILE_PATH,(err,files)=>{
+            if(err) return reject('Unnable to scan'+ err); 	
+            files.forEach((file)=>{
+                return resolve(file);
+                
+            });
+        });
+
+        UploadSftp.on('error', (err)=>{
+            throw err;
+        })
+        .on('uploading', (progress)=>{
+            console.log('Uploading :', progress.file);
+            console.log(progress.percent+ '% completed');
+        })
+        .on('completed', ()=> {
+            console.log('Upload Completed');
+        })
+        .upload();
+    })
+
+}
+
+
+module.exports.getFileNameList = () =>{
+    return new Promise ((resolve,reject)=>{
+        sftp.connect(config)
+        .then(()=>{
+         return sftp.list(REMOTE_URL)
+        .then((data)=>{
+        fileName = []
+        data.forEach((data)=>{
+            fileName.push(data.name);
+        })
+        resolve(fileName);
+        })
+    }).catch((err)=>{
+    reject (err);
+    })
+    });
+}
+
+module.exports.downloadFile = (fileName) => {
+    return REMOTE_URL+fileName;
+}
+
+
+
+
