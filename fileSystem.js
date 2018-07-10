7e8d5b26aa6497c491e4ea472e01e8c4b77637e9
KITSwDevTeam <swdevteam@kit.edu.kh>
2018-07-10 16:54:03 +0700

    add specific time to run cronjob

--- fileSystem.js
+++ fileSystem.js
@@ -35,7 +35,7 @@ const UploadSftp = new SftpUpload(sftp_config);
 module.exports.uploadFile = () =>{
     return new Promise((resolve,reject)=>{
         const hour = new Date();
-        if(hour.getHours()===13 && hour.getMinutes()===55){
+        if(hour.getHours()=== 24 && hour.getMinutes()=== 00){
         fs.readdir(FILE_PATH,(err,files)=>{
             if(err) return reject('Unnable to scan'+ err); 	
             files.forEach((file)=>{
